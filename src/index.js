18c72c9ca0f8126ddd26b52675bc0fac36d60c92
Derek Mercedes <mercedes.derek@gmail.com>
2023-11-15 20:22:54 -0700

    refactoring and backend addition

--- src/index.js
+++ src/index.js
@@ -1,129 +1,151 @@
 document.addEventListener('DOMContentLoaded', () => {
 
-const variantForm = document.getElementById("variant-form")
-variantForm.addEventListener("submit", (e)=>{
+    let defaultRace
+    let current
+    let totalLangs = 0
+    let totalProfs = 0
+
+    //calls the function which populates the custom user origins
+    customMenu()
+
+    //grabs the customization form and adds a listener that accepts user-submitted values
+    const variantForm = document.getElementById("variant-form")
+    variantForm.addEventListener("submit", (e) => {
         e.preventDefault()
+        
         addOrigin(e)
-        let newObject = {
-            ability_bonuses: e.target["#primary-ability"].value,
-            starting_proficiencies: e.target["#proficiencies"].value,
-            languages: e.target["#languages"].value
-        };
-        raceDetails(newObject);
-})
-
-let current
-
-//This is our Get Request from the API
-fetch("https://www.dnd5eapi.co/api/races")
-.then(r=>r.json())
-.then(data => {
-    console.log(data)
-//Iterates Only Race Names: .forEach function to allow objects fetched from API to be iterable
-    data.results.forEach(races =>{
-        raceFiller(races)
     })
-})
-//Races: This function will create our Menu Bar
-function raceMenu(races){
-    let dndRace = document.createElement('h1');
-    dndRace.textContent = races.name;
-    document.querySelector('#wizard-list').append(dndRace);
-//This add event listener function will make the menu bar interactive. When the user clicks on a Race in the menu Bar it will present the race info from the raceDetails function
-dndRace.addEventListener('click',()=>{
-    raceDetails(races)
-   });
-};
-//Iterates Race Data: .forEach that iterates through the data in each race object in the array "races"
-function raceFiller(race){
-    let url = race.url
+
+    const deleteButton = document.querySelector("#delete-button")
+    deleteButton.addEventListener("click", (e)=>{
+        removeCustomOrigin(current)
+    })
 
 
 
-    fetch(`https://www.dnd5eapi.co${url}`)
-    .then(r=>r.json())
-.then(raceData => {
-    let subUrl
-    if(raceData.name === "Elf" || raceData.name === "Gnome" || raceData.name === "Halfling" ){
+    //This is our Get Request from the API
+    fetch("https://www.dnd5eapi.co/api/races")
+        .then(r => r.json())
+        .then(data => {
+            //Iterates Only Race Names: .forEach function to allow objects fetched from API to be iterable
+            data.results.forEach(races => {
+                raceFiller(races)
+            })
+        })
+    //Races: This function will create our Menu Bar
+    function raceMenu(races) {
+        let dndRace = document.createElement('h1');
+        dndRace.textContent = races.name;
+        document.querySelector('#wizard-list').append(dndRace);
+        //This add event listener function will make the menu bar interactive. When the user clicks on a Race in the menu Bar it will present the race info from the raceDetails function
+        dndRace.addEventListener('click', () => {
+            raceDetails(races)
+        });
+        defaultRace = races
+        raceDetails(races)
+    };
+    //Iterates Race Data: .forEach that iterates through the data in each race object in the array "races"
+    //uses the route contained in the "race" object to get the relevant data from the api
+    function raceFiller(race) {
+        let url = race.url
 
-        if(race.name === "Elf"){
-            subUrl = "/api/subraces/high-elf"
-        }else if(race.name === "Gnome"){
-            subUrl = "/api/subraces/rock-gnome"
-        }else if(race.name === "Halfling"){
-            subUrl = "/api/subraces/lightfoot-halfling"
-        }
-        console.log(subUrl)
-        fetch(`https://www.dnd5eapi.co${subUrl}`)
-        .then(res=>res.json())
-        .then(subrace=>{
-            subrace.starting_proficiencies.forEach((prof)=>{
-                raceData.starting_proficiencies.push(prof)
-            })})}
-    raceMenu(raceData)
-}
-)}
 
 
-//Information: This function will present info on each race in the Menu Bar
-function raceDetails(raceInQuestion){
+        fetch(`https://www.dnd5eapi.co${url}`)
+            .then(r => r.json())
+            .then(raceData => {
+                let subUrl
+                //These 3 races have special rules and need a supplemental api call
+                if (raceData.name === "Elf" || raceData.name === "Gnome" || raceData.name === "Halfling") {
 
-    current = raceInQuestion
-    const raceName = document.querySelector("#form-race")
-    let primaryAbilityField = document.querySelector('#primary-ability');
-    let secondaryAbilityField = document.querySelector("#secondary-ability")
-    let languages = document.querySelector('#languages');
-    languages.innerHTML = ""
-    const optionalProfs = document.querySelector(`#optional-profs`)
-    const selectableProfs = document.querySelector(`#selectable-profs`)
-    selectableProfs.innerHTML=""
-    const optionalAbility = document.querySelector(`#optional-ability`)
-    const halfElfAbility = document.querySelector(`#half-elf-ability`)
-    const primaryAbility = raceInQuestion.ability_bonuses[0].ability_score.index
+                    if (race.name === "Elf") {
+                        subUrl = "/api/subraces/high-elf"
+                    } else if (race.name === "Gnome") {
+                        subUrl = "/api/subraces/rock-gnome"
+                    } else if (race.name === "Halfling") {
+                        subUrl = "/api/subraces/lightfoot-halfling"
+                    }
+                    fetch(`https://www.dnd5eapi.co${subUrl}`)
+                        .then(res => res.json())
+                        .then(subrace => {
+                            subrace.starting_proficiencies.forEach((prof) => {
+                                raceData.starting_proficiencies.push(prof)
+                            })
+                        })
+                }
+                raceMenu(raceData)
 
-    if(raceInQuestion.starting_proficiencies[0]){
-    raceInQuestion.starting_proficiencies.forEach((proficiency)=>{
-        const newChoice = document.createElement("tr")
-        const defaultProf = document.createElement("td")
-        const choiceSelect = document.createElement("td")
+            }
+            )
 
-        defaultProf.innerHTML = `${proficiency.name}`
 
-        choiceSelect.innerHTML = `
-        <input type="text> value="" placeholder="Your Choice Here"></input>
+    }
+
+
+    //Information: This function will present info on each race in the Menu Bar
+    function raceDetails(raceInQuestion) {
+        current = raceInQuestion
+        const raceName = document.querySelector("#form-race")
+        let primaryAbilityField = document.querySelector('#primary-ability');
+        let secondaryAbilityField = document.querySelector("#secondary-ability")
+        let languages = document.querySelector('#languages');
+        languages.innerHTML = ""
+        const optionalProfs = document.querySelector(`#optional-profs`)
+        const selectableProfs = document.querySelector(`#selectable-profs`)
+        selectableProfs.innerHTML = ""
+        const optionalAbility = document.querySelector(`#optional-ability`)
+        const halfElfAbility = document.querySelector(`#half-elf-ability`)
+        const primaryAbility = raceInQuestion.ability_bonuses[0].ability_score.index
+        totalProfs = 0
+        //confirms that racial proficiencies exist before populating that section of the form
+        if (raceInQuestion.starting_proficiencies[0]) {
+            raceInQuestion.starting_proficiencies.forEach((proficiency) => {
+                const newChoice = document.createElement("tr")
+                const defaultProf = document.createElement("td")
+                const choiceSelect = document.createElement("td")
+
+                defaultProf.innerHTML = `${proficiency.name}`
+
+                choiceSelect.innerHTML = `
+        <input id="prof-${totalProfs}" type="text value="" placeholder="Your Choice Here"></input>
         `
-        newChoice.append(defaultProf)
-        newChoice.append(choiceSelect)
-        selectableProfs.append(newChoice)
+                totalProfs++
+                newChoice.append(defaultProf)
+                newChoice.append(choiceSelect)
+                selectableProfs.append(newChoice)
 
-        optionalProfs.hidden = false
-    })} else {
-        optionalProfs.hidden = true
-    }
+                optionalProfs.hidden = false
+            })
+        } else {
+            optionalProfs.hidden = true
+        }
 
-    if(raceInQuestion.ability_bonuses[1]){
-        secondaryAbilityField.value = raceInQuestion.ability_bonuses[1].ability_score.index
-        secondaryAbilityField.options[`${raceInQuestion.ability_bonuses[1].ability_score.index}`].innerText = raceInQuestion.ability_bonuses[1].ability_score.index.toUpperCase()
-        optionalAbility.hidden = false
-        halfElfAbility.hidden = true
-    } else if(raceInQuestion.index === "half-elf"){
-        halfElfAbility.hidden = false
-        optionalAbility.hidden = false
-    }else
-    {
-        optionalAbility.hidden = true
-        halfElfAbility.hidden = true
+        //confirms that the race has a secondary (or teritiary) ability bonus before populating the form with it
+        if (raceInQuestion.ability_bonuses[1]) {
+            secondaryAbilityField.value = raceInQuestion.ability_bonuses[1].ability_score.index
+            secondaryAbilityField.options[`${raceInQuestion.ability_bonuses[1].ability_score.index}`].innerText = raceInQuestion.ability_bonuses[1].ability_score.index.toUpperCase()
+            optionalAbility.hidden = false
+            halfElfAbility.hidden = true
+        } else if (raceInQuestion.index === "half-elf") {
+            halfElfAbility.hidden = false
+            optionalAbility.hidden = false
+        } else {
+            optionalAbility.hidden = true
+            halfElfAbility.hidden = true
 
-    }
+        }
 
-    raceInQuestion.languages.forEach((language)=>{
-        const newRow = document.createElement("tr")
-        const newLanguage = document.createElement("td")
+        // all races have languages by default, so this will generate a section on the form
+        // to customize the language, setting the initial values to the racial defaults
+        totalLangs = 0
+        raceInQuestion.languages.forEach((language) => {
+            const newRow = document.createElement("tr")
+            const newLanguage = document.createElement("td")
+            
 
 
-
-        newLanguage.innerHTML = `
-        <select id="default-${language.index}">
+            newLanguage.innerHTML = `
+        <select name="language-${totalLangs}" class="language-${totalLangs}">
             <option value="abyssal">Abyssal</option>
             <option value="celestial">Celestial</option>
             <option value="common">Common</option>
@@ -142,113 +164,151 @@ function raceDetails(raceInQuestion){
             <option value="undercommon">Undercommon</option>
         </select>
         `
-        newRow.append(newLanguage)
-        languages.append(newRow)
-        document.querySelector(`#default-${language.index}`).value=language.index
+            newRow.append(newLanguage)
+            languages.append(newRow)
+            document.querySelector(`.language-${totalLangs}`).value = language.index
+            totalLangs++
+        })
 
-    })
 
 
+        raceName.value = raceInQuestion.name
 
-    raceName.value = raceInQuestion.name
-
-    primaryAbilityField.value = primaryAbility
-    primaryAbilityField.options[`${primaryAbility}`].innerText = primaryAbility.toUpperCase()
-}
-})
-
-function addOrigin(newOrigin){
-
-    console.log(newOrigin)
-    const postObj = {
-        index: `${newOrigin.target["form-race"].value}`,
-        ability_bonuses : [
-            {
-                ability_score: {
-                    index: `${newOrigin.target["primary-ability"].value}`,
-                    name: `${newOrigin.target["primary-ability"].value.slice(0,3).toUpperCase()}`,
-                },
-                bonus : 2
-            },
-            {
-                ability_score: {
-                    index: `${newOrigin.target["secondary-ability"].value}`,
-                    name: `${newOrigin.target["secondary-ability"].value.slice(0,3).toUpperCase()}`,
-                },
-                bonus : 1
-            }
-        ],
-        languages: [
-            {
-                index: `${newOrigin.target["languages"].value}`,
-                name:  `${newOrigin.target["languages"].value}`,
-
-            }
-        ],
-        starting_proficiencies: [
-            {
-                index: `${newOrigin.target["proficiencies"].value}`,
-                name: `${newOrigin.target["proficiencies"].value}`
-            }
-
-        ]
+        primaryAbilityField.value = primaryAbility
+        primaryAbilityField.options[`${primaryAbility}`].innerText = primaryAbility.toUpperCase()
     }
 
-    // const putObj = {
-    //     index : "Kobold",
-    //     ability_bonuses : [
-    //         {
-    //         ability_score: {
-    //             index: "dex",
-    //             name: "DEX",
-    //             url: "api/ability-scores/con"
 
-    //         },
-    //         bonus : 2
-    //          }
-    //     ],
-    //     starting_proficiencies:[
-    //         {index: "biters",
-    //         name: "biters",
-    //         url: "/api/proficiencies/unarmed"}
-    //     ],
-    //     languages : [
-    //         {
-    //             index: "common",
-    //             name : "Common",
-    //             url : "api/languages/common"
-    //         },
-    //         {
-    //             index: "draconic",
-    //             name: "Draconic",
-    //             url : "api/languages/draconic"
-    //         }
-    //     ]
+
+//Accepts the form inputs and adds a new race to the db
+function addOrigin(newOrigin) {
+
+   const langConverted = []
+   const profConverted = []
+    //try - catch syntax is used to make the confirmation function have a response on a failed db write
+    try {
+
+        for(let i = 0; i < totalLangs; i++){
+            const thisLang = newOrigin.target[`language-${i}`].value
+            const langObj = {
+                index : `${thisLang}`,
+                name: `${thisLang.charAt(0).toUpperCase()}` + `${thisLang.slice(1)}`
+            }
+            langConverted.push(langObj)
+        }
+
+        for(let i = 0; i < totalProfs; i++){
+            const thisProf = newOrigin.target[`prof-${i}`].value
+            const profObj = {
+                index: `${thisProf}`,
+                name: `${thisProf.charAt(0).toUpperCase()}` + `${thisProf.slice(1)}`
+            }
+            profConverted.push(profObj)
+            console.log(profObj)
+        }
         
-    // }
 
+        const postObj = {
+            index: `${newOrigin.target["form-race"].value.toLowerCase()}`,
+            name: `${newOrigin.target["form-race"].value}`,
+            ability_bonuses: [
+                {
+                    ability_score: {
+                        index: `${newOrigin.target["primary-ability"].value}`,
+                        name: `${newOrigin.target["primary-ability"].value.slice(0, 3).toUpperCase()}`,
+                    },
+                    bonus: 2
+                },
+                {
+                    ability_score: {
+                        index: `${newOrigin.target["secondary-ability"].value}`,
+                        name: `${newOrigin.target["secondary-ability"].value.slice(0, 3).toUpperCase()}`,
+                    },
+                    bonus: 1
+                }
+            ],
+            languages:  langConverted
+            ,
+            starting_proficiencies: profConverted
+        }
+
+            //adds custom origin to db and shows on-screen feedback on success or failure
     fetch("http://localhost:3000/races/", {
         method: "POST",
         headers: {
-            "Content-Type" : "application/json"
+            "Content-Type": "application/json"
         },
         body: JSON.stringify(postObj)
-    }).then(()=>{
+    }).then(() => {
         showConfirmation("Custom Origin Added!", "green")
-    }).catch((err)=>{
+        raceDetails(postObj)
+        customMenu()
+    }).catch((err) => {
         showConfirmation(err, "red")
     })
+
+    }
+    catch (err) {
+        showConfirmation(err, "red")
+    }
+
+
+
 }
 
-function showConfirmation(message, color){
+//fetches and renders a list of user-created custom origins from the database
+function customMenu() {
+
+    const customList = document.querySelector('#saved-origins-list')
+    customList.innerHTML = ""
+
+    fetch("http://localhost:3000/races")
+        .then(r => r.json())
+        .then(data => {
+
+            data.forEach(race => {
+                const newUserOrigin = document.createElement('h1');
+                newUserOrigin.textContent = race.name;
+                document.querySelector('#saved-origins-list').append(newUserOrigin);
+
+                newUserOrigin.addEventListener('click', () => {
+                    raceDetails(race)
+                })
+            })
+            current = data[data.length-1]
+        })
+
+}
+
+//shows feedback to user before disappearing after 3.5 seconds
+function showConfirmation(message, color) {
     const confirmation = document.getElementById("button-feedback")
     confirmation.textContent = `${message}`
     confirmation.style = `background-color : ${color}`
     confirmation.hidden = false
-    setTimeout(()=>{
+    setTimeout(() => {
         confirmation.hidden = true
     }, 3500)
 }
 
+function removeCustomOrigin(current){
+
+    fetch(`http://localhost:3000/races/${current.id}`, {
+        method: "DELETE"
+    })
+    .then(res=>{
+        if(res.ok){
+            showConfirmation("Custom Origin Deleted", "green")
+        } else{
+            showConfirmation("No matching Custom Origin found", "red")
+        }
+    })
+    .then(()=>{
+        customMenu()
+        raceDetails(defaultRace)
+
+    })
+}
 
 
+})
